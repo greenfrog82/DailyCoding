d1c8ea7a5e62e958ab1619a1843d81cb84c5dea9
greenfrog <greenfrog82@naver.com>
2018-06-17 21:44:33 +0900

    6kyu find the odd int

--- Codewars/6kyu/6kyu_find_the_odd_int/src/main.py
+++ Codewars/6kyu/6kyu_find_the_odd_int/src/main.py
@@ -1,6 +1,6 @@
 import unittest
 
-def find_it(seq):
+def mysolution_1(seq):
     for i, ival in enumerate(seq):
         count = 1
         for j, jval in enumerate(seq):
@@ -11,7 +11,48 @@ def find_it(seq):
             return ival
 
     return None
-             
+
+def mysolution_2(seq):
+    from itertools import groupby
+
+    seq.sort()
+    for key, group in groupby(seq):
+        if 0 != len(list(group)) % 2:
+            return key
+    return None
+
+def mysolution_3(seq):
+    seq.sort()
+    cnt = 1
+    candidate = seq[0]
+
+    for i in range(1, len(seq)):
+        if candidate != seq[i]:
+            if 0 != cnt % 2:
+                break
+            else:
+                candidate = seq[i]
+                cnt = 1
+        else:
+            cnt += 1
+
+    return candidate
+
+def other_solution_1(seq):
+    for i in seq:
+        if seq.count(i)%2 != 0:
+            return i
+
+def other_solution_2(seq):
+    import operator
+    import functools
+
+    return functools.reduce(operator.xor, seq)
+    
+        
+find_it = other_solution_2
+
+
 class Test(unittest.TestCase):
     def test_1(self):
         self.assertEqual(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]), 5)
