59de3429ebabd9db90a738aeebbadd8fde9afdd3
jaeyoung.cho <jaeyoung.cho@cdnetworks.co.kr>
2018-06-18 19:57:43 +0900

    Find the largest consecutive sum of elements

--- mailcoding/find_the_largest_consecutive_sum_of_elements/readme.md
+++ mailcoding/find_the_largest_consecutive_sum_of_elements/readme.md
@@ -8,11 +8,11 @@ Given an integer array, find the largest consecutive sum of elements. You must r
 
 ```python
 Input: [-1, 3, -1, 5]
-Output: 7 // 3 + (-1) + 5
+Output: 7 # 3 + (-1) + 5
 
 Input: [-5, -3, -1]
-Output: -1 // -1
+Output: -1 # -1
 
 Input: [2, 4, -2, -3, 8]
-Output: 9 // 2 + 4 + (-2) + (-3) + 8
+Output: 9 # 2 + 4 + (-2) + (-3) + 8
 ```
--- mailcoding/find_the_largest_consecutive_sum_of_elements/src/main.py
+++ mailcoding/find_the_largest_consecutive_sum_of_elements/src/main.py
@@ -1,22 +1,42 @@
-from collections import OrderedDict
-import functools
 import unittest
 
-def find_the_largest_consecutive_sum_of_elements(inputs):
+def solution(inputs):
+    from itertools import islice
 
+    max_sum = current_sum = inputs[0]
+    
+    for value in islice(inputs, 1, None):
+        current_sum = max(current_sum + value, value)
+        max_sum = max(current_sum, max_sum)
 
+    return max_sum
+        
+        
 class Test(unittest.TestCase):
     def test_1(self):
         # 3 + (-1) + 5
-        self.assertEqual(find_the_largest_consecutive_sum_of_elements([-1, 3, -1, 5]), 7)
+        self.assertEqual(solution([-1, 3, -1, 5]), 7)
 
-    # def test_2(self):
+    def test_2(self):
     #     # -1
-    #     self.assertEqual(find_the_largest_consecutive_sum_of_elements([-5, -3, -1]), -1)
+        self.assertEqual(solution([-5, -3, -1]), -1)
 
-    # def test_3(self):
+    def test_3(self):
     #     # 2 + 4 + (-2) + (-3) + 8
-    #     self.assertEqual(find_the_largest_consecutive_sum_of_elements([2, 4, -2, -3, 8]), 9)
+        self.assertEqual(solution([2, 4, -2, -3, 8]), 9)
+
+    def test_4(self):
+        self.assertEqual(solution([3, -1, -1, 2]), 3)
+    
+    def test_5(self):
+        self.assertEqual(solution([3, -1, -1, 3]), 4)
+
+    def test_6(self):
+        self.assertEqual(solution([-1, -3, -5]), -1)
+
+    def test_7(self):
+        self.assertEqual(solution([2, 4, -2, 2, 8]), 14)
+
         
 if __name__ == '__main__':
     unittest.main()        
