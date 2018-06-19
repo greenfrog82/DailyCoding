71258c17260c3c2782b3e9f7b75fda59e763b14e
jaeyoung.cho <jaeyoung.cho@cdnetworks.co.kr>
2018-06-19 14:11:29 +0900

    8kyu_convert_a_number_to_a_string

---
+++ Codewars/8kyu/8kyu_convert_a_number_to_a_string/src/main.js
@@ -0,0 +1,11 @@
+function mysolution(num) {
+    return num.toString()
+}
+
+function othersolution(num) {
+    return String(num)
+}
+
+numberToString = othersolution
+
+console.log(numberToString(67) === '67')
