9187dfd00405be87c316604f7d3ac01c948594d4
jaeyoung.cho <jaeyoung.cho@cdnetworks.co.kr>
2018-06-19 17:28:02 +0900

    8kyu_find_maximum_minimum_value_of_list

--- Codewars/8kyu/8kyu_find_maximum_minimum_value_of_list/src/main.py
+++ Codewars/8kyu/8kyu_find_maximum_minimum_value_of_list/src/main.py
@@ -1,17 +1,29 @@
-def min(arr):
+def mysolution_min(arr):
     return sorted(arr)[0]
 
-def max(arr):
+def mysolution_max(arr):
     return sorted(arr)[-1] 
 
-print(min([-52, 56, 30, 29, -54, 0, -110]) == -110)
-print(min([42, 54, 65, 87, 0]) == 0)
-print(min([1, 2, 3, 4, 5, 10]) == 1)
-print(min([-1, -2, -3, -4, -5, -10]) == -10)
-print(min([9]) == 9)
+# ------------------------------
 
-print(max([-52, 56, 30, 29, -54, 0, -110]) == 56)
-print(max([4, 6, 2, 1, 9, 63, -134, 566]) == 566)
-print(max([5]) == 5)
-print(max([534, 43, 2, 1, 3, 4, 5, 5, 443, 443, 555, 555]) == 555)
-print(max([9]) == 9)
+def othersolution_min(arr):
+    return min(arr)
+
+def othersolution_max(arr):
+    return max(arr)
+
+
+_min = othersolution_min
+_max = othersolution_max
+
+print(_min([-52, 56, 30, 29, -54, 0, -110]) == -110)
+print(_min([42, 54, 65, 87, 0]) == 0)
+print(_min([1, 2, 3, 4, 5, 10]) == 1)
+print(_min([-1, -2, -3, -4, -5, -10]) == -10)
+print(_min([9]) == 9)
+
+print(_max([-52, 56, 30, 29, -54, 0, -110]) == 56)
+print(_max([4, 6, 2, 1, 9, 63, -134, 566]) == 566)
+print(_max([5]) == 5)
+print(_max([534, 43, 2, 1, 3, 4, 5, 5, 443, 443, 555, 555]) == 555)
+print(_max([9]) == 9)
