c06e7eff06a9dfbae414dcb1e247fbccba189aec
greenfrog <greenfrog82@naver.com>
2018-06-19 21:16:37 +0900

    8kyu_grasshopper-terminal_game_combat_function

--- Codewars/8kyu/8kyu_grasshopper-terminal_game_combat_function/src/main.js
+++ Codewars/8kyu/8kyu_grasshopper-terminal_game_combat_function/src/main.js
@@ -2,7 +2,11 @@ function mysolution(health, damage) {
     return health > damage? health - damage: 0
 }
 
-combat = mysolution
+function othersolution(health, damage) {
+    return Math.max(0, health - damage)
+}
+
+combat = othersolution
 
 console.log(combat(100, 5) === 95)
 console.log(combat(92, 8) === 84)
