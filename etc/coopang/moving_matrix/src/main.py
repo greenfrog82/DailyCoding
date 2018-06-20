6c041fabf74a99d9f1a53b90fb7a27c7edf1c160
greenfrog <greenfrog82@naver.com>
2018-06-21 08:20:38 +0900

    Moving matrix

--- etc/coopang/moving_matrix/src/main.py
+++ etc/coopang/moving_matrix/src/main.py
@@ -23,39 +23,27 @@ def get_moving_indexes(inputs):
 
     return moving_index
 
-def convert_array(inputs):
-    return [j for i in range(len(inputs)) for j in inputs[i]]
-
 def solution(inputs, k):
     moving_indexes = get_moving_indexes(inputs)
-    converted_inputs = convert_array(inputs)
+    converted_inputs = [j for i in range(len(inputs)) for j in inputs[i]]
     cnt = len(moving_indexes)
 
-    tmp = None
-    for i in range(len(moving_indexes)):
+    tmp_ret = [i for i in converted_inputs]
+    for i, src_idx in enumerate(moving_indexes):
         moving_idx = i + k
         if moving_idx >= cnt:
             moving_idx -= cnt
         
-        target_idx = moving_indexes[moving_idx]
-
-        if tmp:
-            x = converted_inputs[target_idx]
-            converted_inputs[target_idx] = tmp
-            tmp = x
-        else:
-            tmp = converted_inputs[target_idx]
-            converted_inputs[target_idx] = converted_inputs[moving_indexes[i]] 
+        dest_idx = moving_indexes[moving_idx]
+        tmp_ret[dest_idx] = converted_inputs[src_idx]
         
-    console.log(converted_inputs)
     ret = []
     tmp = []
-    for i, item in enumerate(converted_inputs):
-        if i + 1 % cnt:
+    for i, item in enumerate(tmp_ret):
+        tmp.append(item)
+        if 0 == (i+1) % len(inputs[0]):
             ret.append(tmp)
             tmp = []
-        else:
-            tmp.append(item)
 
     return ret
      
@@ -67,14 +55,11 @@ class Test(unittest.TestCase):
     def test_get_moving_indexes_2(self):
         self.assertEqual(get_moving_indexes([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]), [0, 1, 2, 3, 7, 11, 15, 14, 13, 12, 8, 4])
 
-    def test_convert_array(self):
-        self.assertEqual(convert_array([[1,2,3],[5,6,7],[9,10,11]]), [1, 2, 3, 5, 6, 7, 9, 10, 11])
-
     def test_solution_1(self):
         self.assertEqual(solution([[1,2,3],[5,6,7],[9,10,11]], 3), [[10,9,5],[11,6,1],[7,3,2]])
 
-    # def test_solution_2(self):
-    #     self.assertEqual(solution([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],2 ), [[9,5,1,2],[13,6,7,3],[14,10,11,4],[15,16,12,8]])
+    def test_solution_2(self):
+        self.assertEqual(solution([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],2 ), [[9,5,1,2],[13,6,7,3],[14,10,11,4],[15,16,12,8]])
 
 
 if __name__ == '__main__':
