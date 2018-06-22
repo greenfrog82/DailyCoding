6b6f795679718c22496f23b9aaf6232c0a88336a
greenfrog <greenfrog82@naver.com>
2018-06-22 17:12:36 +0900

    5kyu sum of pairs

---
+++ Codewars/5kyu/5kyu_sum_of_pairs/src/main.py
@@ -0,0 +1,39 @@
+import unittest
+
+def mysolution(inputs, result):
+    pass
+
+sum_pairs = mysolution
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        l1 = [1, 4, 8, 7, 3, 15]
+        self.assertEqual(sum_pairs(l1, 8) , [1, 7])
+    
+    def test_2(self):
+        l2 = [1, -2, 3, 0, -6, 1]
+        self.assertEqual(sum_pairs(l2, -6) , [0, -6])
+
+    def test_3(self):
+        l3 = [20, -13, 40]
+        self.assertEqual(sum_pairs(l3, -7) , None)
+
+    def test_4(self):
+        l4 = [1, 2, 3, 4, 1, 0]
+        self.assertEqual(sum_pairs(l4, 2) , [1, 1])
+
+    def test_5(self):
+        l5 = [10, 5, 2, 3, 7, 5]
+        self.assertEqual(sum_pairs(l5, 10) , [3, 7])        
+
+    def test_6(self):
+        l6 = [4, -2, 3, 3, 4]
+        self.assertEqual(sum_pairs(l6, 8) , [4, 4])
+
+    def test_7(self):
+        l7 = [0, 2, 0]
+        self.assertEqual(sum_pairs(l7, 0) , [0, 0])
+
+    def test_8(self):
+        l8 = [5, 9, 13, -3]
+        self.assertEqual(sum_pairs(l8, 10) , [13, -3])
