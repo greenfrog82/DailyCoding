a04edee5e3f5a810ec7082b8d13da46f4a949aa8
greenfrog <greenfrog82@naver.com>
2018-06-25 09:50:42 +0900

    5kyu sum of pairs

--- Codewars/5kyu/5kyu_sum_of_pairs/src/main.py
+++ Codewars/5kyu/5kyu_sum_of_pairs/src/main.py
@@ -38,30 +38,29 @@ def mysolution_for_python3(inputs, result):
     
     return [ret[-1][0], ret[-1][1][0]]
 
-def mysolution_for_python2(inputs, result):
-    dic = {result-val:val for val in inputs}
-    dic = {key:dic.get(key) for key in inputs if None != dic.get(key)}
+from collections import OrderedDict
 
-    print(dic)
+def mysolution_for_python2(inputs, result):
+    tmpdic = {result-val:val for val in inputs}
+    dic = OrderedDict()
+    
+    for key in inputs:
+        if None != tmpdic.get(key):
+            dic[key] = tmpdic.get(key)
+
     if not dic:
         return None
 
-    del_candidate_keys = {}
-    
-    # for key in inputs:
-    #     k = key
-    #     v = dic.get(key)
+    be_deleted_keys = {}
+    for k,v in dic.iteritems():
+        if None == be_deleted_keys.get(k) and k != v and None != dic.get(v): 
+            be_deleted_keys[v] = k
 
-    #     if k != v and None != dic.get(v) and None == del_candidate_keys.get(k):
-    #         del_candidate_keys[v] = v
+    for k in be_deleted_keys.iterkeys():
+        del dic[k]
 
-    # for k in del_candidate_keys.keys():
-    #     del dic[k]
-
-
-
-    dic_with_distance = {}
-    for k, v in dic.items():
+    dic_with_distance = OrderedDict()
+    for k, v in dic.iteritems():
         if k == v:
             last_indices = []
             last_idx = -1
@@ -79,42 +78,42 @@ def mysolution_for_python2(inputs, result):
 
     ret = sorted(dic_with_distance.items(), key=lambda x: x[1])
     
-    # return [ret[-1][0], ret[-1][1][0]]
+    return [ret[-1][0], ret[-1][1][0]]
 
-sum_pairs = mysolution_for_python3
+sum_pairs = mysolution_for_python2
 
 class Test(unittest.TestCase):
     def test_1(self):
         l1 = [1, 4, 8, 7, 3, 15]
         self.assertEqual(sum_pairs(l1, 8) , [1, 7])
     
-    # def test_2(self):
-    #     l2 = [1, -2, 3, 0, -6, 1]
-    #     self.assertEqual(sum_pairs(l2, -6) , [0, -6])
+    def test_2(self):
+        l2 = [1, -2, 3, 0, -6, 1]
+        self.assertEqual(sum_pairs(l2, -6) , [0, -6])
 
-    # def test_3(self):
-    #     l3 = [20, -13, 40]
-    #     self.assertEqual(sum_pairs(l3, -7) , None)
+    def test_3(self):
+        l3 = [20, -13, 40]
+        self.assertEqual(sum_pairs(l3, -7) , None)
 
-    # def test_4(self):
-    #     l4 = [1, 2, 3, 4, 1, 0]
-    #     self.assertEqual(sum_pairs(l4, 2) , [1, 1])
+    def test_4(self):
+        l4 = [1, 2, 3, 4, 1, 0]
+        self.assertEqual(sum_pairs(l4, 2) , [1, 1])
 
-    # def test_5(self):
-    #     l5 = [10, 5, 2, 3, 7, 5]
-    #     self.assertEqual(sum_pairs(l5, 10) , [3, 7])        
+    def test_5(self):
+        l5 = [10, 5, 2, 3, 7, 5]
+        self.assertEqual(sum_pairs(l5, 10) , [3, 7])        
 
-    # def test_6(self):
-    #     l6 = [4, -2, 3, 3, 4]
-    #     self.assertEqual(sum_pairs(l6, 8) , [4, 4])
+    def test_6(self):
+        l6 = [4, -2, 3, 3, 4]
+        self.assertEqual(sum_pairs(l6, 8) , [4, 4])
 
-    # def test_7(self):
-    #     l7 = [0, 2, 0]
-    #     self.assertEqual(sum_pairs(l7, 0) , [0, 0])
+    def test_7(self):
+        l7 = [0, 2, 0]
+        self.assertEqual(sum_pairs(l7, 0) , [0, 0])
 
-    # def test_8(self):
-    #     l8 = [5, 9, 13, -3]
-    #     self.assertEqual(sum_pairs(l8, 10) , [13, -3])
+    def test_8(self):
+        l8 = [5, 9, 13, -3]
+        self.assertEqual(sum_pairs(l8, 10) , [13, -3])
 
     def test_9(self):
         # data = [random.randint(1, 100) for i in range(10000000)]
@@ -123,13 +122,13 @@ class Test(unittest.TestCase):
         # file.write(','.join(str(i) for i in data))
         # file.close()
 
-        # file = open('data.txt', 'r')
-        # content = file.read().split(',')
-        # file.close()
+        file = open('data.txt', 'r')
+        content = file.read().split(',')
+        file.close()
 
-        # data = [int(i) for i in content]
+        data = [int(i) for i in content]
 
-        # sum_pairs(data, 10)
+        sum_pairs(data, 10)
         
         
     
