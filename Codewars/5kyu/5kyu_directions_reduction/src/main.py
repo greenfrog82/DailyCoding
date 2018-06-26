9c5de9504096c3a5fa913a53c05b8825c60f2f6c
greenfrog <greenfrog82@naver.com>
2018-06-26 16:25:05 +0900

    5kyu directions reduction

--- Codewars/5kyu/5kyu_directions_reduction/src/main.py
+++ Codewars/5kyu/5kyu_directions_reduction/src/main.py
@@ -3,7 +3,7 @@ import unittest
 def mysolution(arr):
     if not arr:
         return arr
-        
+
     stack = [arr[0]]
     
     for i in range(1, len(arr)):
@@ -19,7 +19,25 @@ def mysolution(arr):
 
     return stack
 
-dirReduc = mysolution
+def othersolution(arr):
+    opposite = {'NORTH':'SOUTH', 'EAST':'WEST', 'SOUTH':'NORTH', 'WEST':'EAST'}
+    new_plan = []
+
+    for d in arr:
+        if new_plan and opposite[d] == new_plan[-1]:
+            new_plan.pop()
+        else:
+            new_plan.append(d)
+
+    return new_plan
+
+def othersolution2(arr):
+    str = ' '.join(arr).replace('NORTH SOUTH', '').replace('EAST WEST', '').replace('SOUTH NORTH', '').replace('WEST EAST', '')
+    new_plan = [d for d in str.split(' ') if d] if str else []
+    return new_plan if len(new_plan) == len(arr) else othersolution2(new_plan)
+    
+    
+dirReduc = othersolution2
 
 class Test(unittest.TestCase):
     def test_1(self):
@@ -29,8 +47,20 @@ class Test(unittest.TestCase):
         self.assertEqual(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]), ["NORTH", "WEST", "SOUTH", "EAST"])
 
     def test_3(self):
+        self.assertEqual(dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'NORTH', 'SOUTH', 'NORTH', 'WEST', 'EAST']), ['NORTH', 'NORTH'])
+
+    def test_4(self):
         self.assertEqual(dirReduc([]), [])
-    
+
+    def test_5(self):
+        self.assertEqual(dirReduc(['EAST', 'NORTH', 'EAST', 'WEST']), ['EAST', 'NORTH'])
+
+    def test_6(self):
+        self.assertEqual(dirReduc(['EAST', 'EAST', 'WEST', 'NORTH', 'WEST', 'EAST', 'EAST', 'SOUTH', 'NORTH', 'WEST']), ['EAST', 'NORTH'])
+
+    def test_7(self):
+        self.assertEqual(dirReduc(['NORTH', 'EAST', 'NORTH', 'EAST', 'WEST', 'WEST', 'EAST', 'EAST', 'WEST', 'SOUTH']), ['NORTH', 'EAST'])
+
 
 if __name__ == '__main__':
     unittest.main()
