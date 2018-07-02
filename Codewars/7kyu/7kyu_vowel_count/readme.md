d53a96337b8687fcfabaf842e8cfdf72941c21a9
greenfrog <greenfrog82@naver.com>
2018-07-02 19:51:09 +0900

    7kyu vowel count

---
+++ Codewars/7kyu/7kyu_vowel_count/readme.md
@@ -0,0 +1,7 @@
+# [7kyu Vowel Count](https://www.codewars.com/kata/vowel-count/python)
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters and/or spaces.
---
+++ Codewars/7kyu/7kyu_vowel_count/src/main.py
@@ -0,0 +1,21 @@
+import unittest
+
+def mysolution(inputStr):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    num_vowels = 0
+    for vowel in vowels:
+        num_vowels += inputStr.count(vowel)
+    return num_vowels
+
+def othersolution(inputStr):
+    return sum(c in 'aeiou' for c in inputStr)
+
+getCount = othersolution
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(getCount('abracadabra'), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
